e6190a53a2e4860a8c9c300020cca3165e875697
Raphael Akpofure <blizywap@gmail.com>
2022-02-11 16:07:30 +0100

    added search function

--- market.php
+++ market.php
@@ -4,21 +4,30 @@
  
  ?>
  
- <div class="conteunt containeru" style="padding-top: 1em;">
+ <div class="conteunt d-container" style="padding-top: 1em;">
  <div class="container">
+	 <form method="GET" action="market">
  <div class="input-group md-form form-sm form-1 pl-0">
  <div class="input-group-prepend">
- <span class="input-group-text purple lighten-3" id="basic-text1"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
- </span>
+ <button class="input-group-text purple lighten-3" id="basic-text1"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/></svg>
+</button>
  </div>
- <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search">
+ <input name="search" class="form-control my-0 py-1" type="text" value="<?php if(isset($_GET['search'])){echo $_GET['search'];}?>" placeholder="Search" aria-label="Search">
+</form>
   <button class="btn"><svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 17v2h6v-2H3zM3 5v2h10V5H3zm10 16v-2h8v-2h-8v-2h-2v6h2zM7 9v2H3v2h4v2h2V9H7zm14 4v-2H11v2h10zm-6-4h2V7h4V5h-4V3h-2v6z"/></svg>
   Filter</button>
  <button class="btn">Add</button>
  </div>
  </div>
  <?php
+ // $sql="select * from information where First_Name like '%$search_value%'";
+ if(isset($_GET["search"])){
+ $query = $_GET["search"];
+  $sql = "SELECT users.id, transaction.itemName, transaction.price, transaction.itemDesc, transaction.created_at, users.name, users.email, users.badge, users.photo FROM transaction INNER JOIN users ON transaction.uid = users.id WHERE transaction.itemName LIKE '%$query%' AND  transaction.list = 1 ORDER BY transaction.created_at DESC";
+ }
+ else{
  $sql = "SELECT users.id, transaction.itemName, transaction.price, transaction.itemDesc, transaction.created_at, users.name, users.email, users.badge, users.photo FROM transaction INNER JOIN users ON transaction.uid = users.id WHERE transaction.list = 1 ORDER BY transaction.created_at DESC";
+ }
  $result = $conn->query($sql);
  
  if ($result->num_rows > 0) {
@@ -81,6 +90,14 @@
 	include('assets/php/footer.php');
 	?>
 	<style>
+			@media screen and (min-width: 768px) {
+				.d-container {
+					width:50%;
+					margin: 0 auto;
+}
+ }
+
+		}
 	.post-card a{
 	color:#000;
 	}
